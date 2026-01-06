533fecb92e692f82ab49cf53687e671657a578ca
ayush02303 <badolaayush295@gmail.com>
2026-01-06 20:21:21 +0530

    firrst

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("First program");
+        System.err.println("Hello again! buddy");
+        
+    }
+}
