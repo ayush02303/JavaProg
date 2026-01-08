b5120e407ce8fc242ae90a6384b23b6dd3c31963
ayush02303 <badolaayush295@gmail.com>
2026-01-08 11:50:50 +0530

    method overloading

--- classesAndObjects.java
+++ classesAndObjects.java
@@ -1,16 +1,33 @@
 class Add{
 
-    public void show(){
+    
+    public void show( int n1 , int n2){
         System.out.println("under show!");
+        System.out.println(n1+n2);
     }
 
+    //having a return type as string 
+    public String printName( String name ){
+        if( name.length() > 5){
+            return "Hello , Mr"+name; 
+        }
+        else{
+            return "names needs to be a little longer"; 
+        }
+
+    }
 }
 
 
 public class classesAndObjects {
     public static void main(String[] args) {
+        int num1 = 122; 
+        int num2 = 1222; 
+
         Add obj = new Add(); 
-        obj.show(); 
+        obj.show(num1 , num2); 
+        String result = obj.printName("Ayush");
+        System.out.println(result);
         
     }
 }
---
+++ methodOverloading.java
@@ -0,0 +1,31 @@
+class functionOverloading{
+
+
+    // Method overloading means having multiple methods with the same name in the same class, but they do different things based on the input you give them.
+    public int sum( int val1 , int val2 , int val3){
+        return val1+val2+val3; 
+
+    }
+
+    public int sum( int val1 , int val2){
+        return val1+ val2; 
+
+    }
+
+}
+public class methodOverloading {
+    
+    public static void main(String[] args) {
+        functionOverloading obj = new functionOverloading(); 
+        int result1 = obj.sum(12, 23); 
+        int result2 = obj.sum(12, 2,12); 
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+
+
+        
+    }
+    
+}
