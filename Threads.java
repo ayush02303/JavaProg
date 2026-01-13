af6d1b6c9d4632531d32d4435a0400d10fdf57cc
ayush02303 <badolaayush295@gmail.com>
2026-01-13 14:03:44 +0530

    threads

---
+++ Threads.java
@@ -0,0 +1,25 @@
+class A extends Thread {
+    public void run() {
+        for(int i = 0; i < 100; i++) {
+            System.out.println("under A");
+        }
+    }
+}
+
+class B extends Thread {
+    public void run() {
+        for(int i = 0; i < 100; i++) {
+            System.out.println("under B");
+        }
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        A obj = new A();
+        B obj2 = new B();
+
+        obj.start();
+        obj2.start();
+    }
+}
